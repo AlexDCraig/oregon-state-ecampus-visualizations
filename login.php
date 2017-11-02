39f216969f63332b8cb7f07736e77fddbbaf55e3
AlexDHoffer <hoffera@oregonstate.edu>
2017-11-02 14:52:38 -0700

    ...

--- login.php
+++ login.php
@@ -5,6 +5,28 @@
 		<link rel="stylesheet" href="index.css">
 	</head>
 <body>
+
+<div class="navHeader">
+<div class="dropdown">
+	<button class="dropbtn"><a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/home.php">Home</a></button>
+</div>
+
+<!-- <a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/account.php">Account</a> !-->
+<div class="dropdown">
+	<button class="dropbtn">Account</button>
+	<div class="dropdown-content">
+		<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/login.php">Login</a>
+		<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/signup.php">Signup</a>
+	</div>
+</div>
+
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/voteOnCourse.php">Vote on Course</a>
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/discussACourse.php">Discuss a Course</a>
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/visualizedCourseComparison.php">Visualized Course Comparison</a>
+</div>
+
+<div class="titleHeader">
+
 <h2>User Log In Page</h2>
 
 <form method="post" action="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/verifyLogin.php">
@@ -12,10 +34,6 @@
 	<input type="password" name="password" />password</br/>
 	<input type="submit" value="Submit">
 </form>	
-
-<br>
-<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/signup.php">Sign Up</a>
-<br>
 
 <?php
 	include 'connectvarsEECS.php';
--- signup.php
+++ signup.php
@@ -5,6 +5,31 @@
 		<link rel="stylesheet" href="index.css">
 	</head>
 <body>
+
+<div class="navHeader">
+<div class="dropdown">
+	<button class="dropbtn"><a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/home.php">Home</a></button>
+</div>
+
+<!-- <a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/account.php">Account</a> !-->
+<div class="dropdown">
+	<button class="dropbtn">Account</button>
+	<div class="dropdown-content">
+		<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/login.php">Login</a>
+		<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/signup.php">Signup</a>
+	</div>
+</div>
+
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/voteOnCourse.php">Vote on Course</a>
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/discussACourse.php">Discuss a Course</a>
+<a href="http://web.engr.oregonstate.edu/~hoffera/cs340/Final/visualizedCourseComparison.php">Visualized Course Comparison</a>
+</div>
+
+<div class="titleHeader">
+
+
+
+
 <h2>User Sign Up Page</h2>
 <form method="post" action="insert.php">
 	<input type="text" name="email" />Email</br/>
